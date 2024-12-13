122d43db8a6f1bbca8de27d9386721b9fc0a4338
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-12-14 00:18:04 +0530

    feature-contact form completed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,107 @@
-
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return (
-    <div>
-      contact us
-    </div>
-  )
-}
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const formRef = useRef();
+  const [isLoading, setisLoading] = useState(false);
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setisLoading(true);
 
-export default Contact
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "sameershamsudheen",
+        from_email: form.email,
+        to_email: "sameerfreelancer97@gmail.com",
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(()=>{
+      setisLoading(false)
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+       //todo:show  success message
+       //Todo: Hide an alert
+    }).catch((error)=>{
+       setisLoading(false)
+       console.log(error);
+       //show ERROR
+    })
+  };
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form   ref={formRef} className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="John@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows="4"
+              className="textarea"
+              placeholder="Write your thoughts here..."
+              value={form.message}
+              required
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            disabled={isLoading}
+            onBlur={handleBlur}
+            className="btn"
+            onFocus={handleFocus}
+            type="submit"
+          >
+            {isLoading ? "Sending...." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
