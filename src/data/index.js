6e1cfb63ab6a84fcb924fc87cd371a8b4b3439e2
sameernshamsudheen <sameernshamsudheen@gmail.com>
2024-12-14 20:36:51 +0530

    fix -data change

--- src/data/index.js
+++ src/data/index.js
@@ -171,49 +171,31 @@ export const projects = [
   {
     iconUrl: pricewise,
     theme: "btn-back-red",
-    name: "Amazon Price Tracker",
+    name: "benatkinson method",
     description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
+      "learning management system for  equestrian sports using react redux-tool node.js strapi.js ",
   },
   {
     iconUrl: threads,
     theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
+    name: "Multi vendor ecommerce  Backend",
     description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
+      "Created a Backend App using  express.js node.js where different vendors can  signup and login upload there products users can  buy this  products, database used is mongodb,redis for caching and razorpay for payment.this project includes adavanced logging and error handling  ",
+    link: "https://github.com/sameernshamsudheen/multivendorBackend",
   },
   {
     iconUrl: car,
     theme: "btn-back-blue",
-    name: "Car Finding App",
+    name: "job portal",
     description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+      "Created a job portal app  with  react supabase and  clerk authentication",
+    link: "https://github.com/sameernshamsudheen/jobportal",
   },
   {
     iconUrl: snapgram,
     theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
-  {
-    iconUrl: estate,
-    theme: "btn-back-black",
-    name: "Real-Estate Application",
-    description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
-    iconUrl: summiz,
-    theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
-    description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+    name: "Single Vendor Learning management system",
+    description: "Backend application with node.js and express.js ",
+    link: "https://github.com/sameernshamsudheen/lmsbackendproject",
   },
 ];
